75ccf427b75d4a1391054c5161359fa1dde1f54c
Adrian Krygowski <adrian.krygowski@wowinc.com>
2021-01-06 16:39:09 -0600

    manual_push

--- steps/newMgmtIP_BDD.py
+++ steps/newMgmtIP_BDD.py
@@ -7,20 +7,24 @@ import wcommon as wc
 wc.jenkins_header(); # load inputs from Jenkinsfile
 wc.wcheader['packages']['wc'] = wc.__file__
 wc.jd(wc.wcheader)
-wc.jd(wc.env_dict)
+# wc.jd(wc.env_dict)
 
 @given(u'Bootstrap Ran')
 def step_impl(context):
-	assert True
+        project = Stc.init('traffictest.py')
+        Stc.connectChassis(ARC)
+        assert True
 
 @when(u'I try to ping NewMgmtIP from Jenkinsfile-paramter-input cidr')
 def step_impl(context):
-	context.ip = wc.env_dict['cidr'].split('/')[0]
-	context.pingable = wc.bdd_bool_inp(wc.is_pingable(context.ip))
-	assert True
+	ip = wc.env_dict['cidr'].split('/')[0] 
+        context.ip = ip
+        context.pingable = bool(wc.is_pingable(ip))
+        pass
 
 @then(u'I expect response "{expectationBoolean}"')
 def step_impl(context, expectationBoolean):
-	expectationBoolean = wc.bdd_bool_inp(expectationBoolean)
-	print('\t'.join(['',context.ip,'actual:' + str(context.pingable),'','expected:' + str(expectationBoolean)]))
-	assert (context.pingable == expectationBoolean)
+        expectationBoolean = wc.bdd_bool_inp(expectationBoolean)
+        print('\t'.join(['',context.ip,'actual:' + str(context.pingable),'','expected:' + str(expectationBoolean)]))
+        assert context.pingable == expectationBoolean
+
