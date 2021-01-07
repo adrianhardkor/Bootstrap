ec857e558c39724164ac78e57d7c413b358371b4
Adrian Krygowski <adrian.krygowski@wowinc.com>
2021-01-07 16:14:12 -0600

    manual_push

--- src/ts_show.tcl
+++ src/ts_show.tcl
@@ -2,38 +2,12 @@
 source "./lib/shared_libs/wc.tcl"
 set_debug 1
 
-proc cred_array {varName} {
-  upvar 1 $varName argv_array_inside
-  global cred
-  array unset cred
-  foreach type $argv_array_inside(User_Pass_Json) {
-    set type [split $type ",/"]
-    switch -- [llength $type] {
-      3 {
-        set cred([lindex $type 0],user) [lindex $type 1]
-        set cred([lindex $type 0],pass) [lindex $type 2]
-      }
-      4 {
-        set cred([lindex $type 0],user) [lindex $type 1]
-        set cred([lindex $type 0],pass) [lindex $type 2]
-        set cred([lindex $type 0],pass15) [lindex $type 3]
-      }
-    }
-  }
-}
-
 global argv_array
 global cred
-cred_array argv_array
 echo_param [array get argv_array]
-echo_param [array get cred]
-exit 0
+cred_array argv_array
 
-set ts_ip [lindex $argv 0]
-set ts_user "arcadmin"
-set ts_pass1 "arcaccess"
-set ts_pass2 "arcenable"
-set ports [mrvTS_show $argv_array("lx_ip") $ts_user $ts_pass1 $ts_pass2 ""]
+set ports [mrvTS_show $argv_array("lx_ip") $cred_array("LX,user") $cred_array("LX,pass") $cred_array("LX,pass15") "portinfohere"]
 puts $ports
 exit 0
 
