4de336b03068e97410011f650e6194debfa9b5c9
Adrian Krygowski <adrian.krygowski@wowinc.com>
2021-01-07 16:30:48 -0600

    manual_push

--- src/ts_show.tcl
+++ src/ts_show.tcl
@@ -7,8 +7,9 @@ global cred
 echo_param [array get argv_array]
 cred_array argv_array
 puts "\n\n\n"
+parray_slow argv_array
 
-set ports [mrvTS_show $argv_array("lx_ip") $cred_array("LX,user") $cred_array("LX,pass") $cred_array("LX,pass15") "portinfohere"]
+set ports [mrvTS_show $argv_array(lx_ip) $cred_array(LX,user) $cred_array(LX,pass) $cred_array(LX,pass15) "portinfohere"]
 puts $ports
 exit 0
 
