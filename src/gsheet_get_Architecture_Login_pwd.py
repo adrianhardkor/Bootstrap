d86e7866224fc3abd9ad8f13f67e04f21006d4b7
Adrian Krygowski <adrian.krygowski@wowinc.com>
2021-01-07 16:08:38 -0600

    manual_push

--- src/gsheet_get_Architecture_Login_pwd.py
+++ src/gsheet_get_Architecture_Login_pwd.py
@@ -24,5 +24,4 @@ out = []
 for a in sheet.keys():
 	out.append(','.join([a.replace(' ',''),sheet[a]['Login'].replace(' ',''),sheet[a]['pwd'].replace(' ','')]))
 print('='.join(out))
-wc.jd(sheet)
 exit(0)
--- src/ts_show.tcl
+++ src/ts_show.tcl
@@ -2,20 +2,18 @@
 source "./lib/shared_libs/wc.tcl"
 set_debug 1
 
-# argv_array(User_Pass_Json) =  EX/MX/PTX,root,ArcLabRoot LX,arcadmin,arcaccess Lepton,lepton,Lepton! CMTS,root,wowlabs12 Spirent,admin,spt_admin Velocity,Userid,ArcWow1 Velocityadmin,admin,wowlabs12 SomeJunipers,ADMIN,ArcLabAdmin
-
 proc cred_array {varName} {
   upvar 1 $varName argv_array_inside
   global cred
   array unset cred
   foreach type $argv_array_inside(User_Pass_Json) {
-    set type [split $type ","]
+    set type [split $type ",/"]
     switch -- [llength $type] {
-      2 {
+      3 {
         set cred([lindex $type 0],user) [lindex $type 1]
         set cred([lindex $type 0],pass) [lindex $type 2]
       }
-      3 {
+      4 {
         set cred([lindex $type 0],user) [lindex $type 1]
         set cred([lindex $type 0],pass) [lindex $type 2]
         set cred([lindex $type 0],pass15) [lindex $type 3]
@@ -27,7 +25,7 @@ proc cred_array {varName} {
 global argv_array
 global cred
 cred_array argv_array
-echo_param [array get arv_array]
+echo_param [array get argv_array]
 echo_param [array get cred]
 exit 0
 
