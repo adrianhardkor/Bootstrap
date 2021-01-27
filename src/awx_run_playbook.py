cbebc955e7ffdf41af356c5ba3865525f13bbe16
Adrian <adrian@Adrians-MacBook-Pro.local>
2021-01-27 11:09:08 -0600

    manual_push

--- src/awx_run_playbook.py
+++ src/awx_run_playbook.py
@@ -15,4 +15,4 @@ ansible = awx.AWX(os.environ['AWX_IP'], os.environ['AWX_USER'], os.environ['AWX_
 # Input/Overload variables == BUBBLED TO JENKINS PARAMETERS (ENV|os.environ PROVIDED VIA JENKINSFILE)
   # argv provided on orchPy paramater
   # env provided via jenkins global and non-orchPy parameters
-out = ansible.RunPlaybook(wc.argv_dict['Playbook'],args=wc.env_dict)
+result,out = ansible.RunPlaybook(wc.argv_dict['Playbook'],args=wc.env_dict)
